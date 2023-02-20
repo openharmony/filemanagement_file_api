041aed8e8acd7bd6b2833a72cf04a7e23f04e594
18721213663 <zhuhongtao12@huawei.com>
2023-02-20 11:19:11 +0800

    bugfix_fs_register
    Signed-off-by: 18721213663 <zhuhongtao12@huawei.com>

--- interfaces/kits/js/src/mod_fs/module.cpp
+++ interfaces/kits/js/src/mod_fs/module.cpp
@@ -49,7 +49,20 @@ static napi_value Export(napi_env env, napi_value exports)
     return exports;
 }
 
-NAPI_MODULE(fs, Export)
+static napi_module _module = {
+    .nm_version = 1,
+    .nm_flags = 0,
+    .nm_filename = nullptr,
+    .nm_register_func = Export,
+    .nm_modname = "file.fs",
+    .nm_priv = ((void *)0),
+    .reserved = {0}
+};
+
+extern "C" __attribute__((constructor)) void RegisterModule(void)
+{
+    napi_module_register(&_module);
+}
 } // namespace ModuleFileIO
 } // namespace FileManagement
 } // namespace OHOS
