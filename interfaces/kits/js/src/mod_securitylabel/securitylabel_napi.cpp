affcfde906d5fd274b8847e25c0f920ecb9df8e7
18721213663 <zhuhongtao12@huawei.com>
2023-04-12 10:21:39 +0800

    fixed 19c8abf from https://gitee.com/zhuhongtao66/filemanagement_file_api/pulls/245 bugfix_fileapi_pluginname
    Signed-off-by: 18721213663 <zhuhongtao12@huawei.com>

--- interfaces/kits/js/src/mod_securitylabel/securitylabel_napi.cpp
+++ interfaces/kits/js/src/mod_securitylabel/securitylabel_napi.cpp
@@ -33,7 +33,20 @@ napi_value SecurityLabelExport(napi_env env, napi_value exports)
     return exports;
 }
 
-NAPI_MODULE(securitylabel, SecurityLabelExport)
+static napi_module _module = {
+    .nm_version = 1,
+    .nm_flags = 0,
+    .nm_filename = nullptr,
+    .nm_register_func = SecurityLabelExport,
+    .nm_modname = "file.securityLabel",
+    .nm_priv = ((void *)0),
+    .reserved = {0}
+};
+
+extern "C" __attribute__((constructor)) void RegisterModule(void)
+{
+    napi_module_register(&_module);
+}
 } // namespace ModuleSecurityLabel
 } // namespace FileManagement
 } // namespace OHOS
