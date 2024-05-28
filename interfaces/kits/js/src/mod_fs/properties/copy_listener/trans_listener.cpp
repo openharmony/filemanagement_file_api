bf4594becd4f95aa7ee5486a59c967cd1d92d18d
lixiang <lixiang493@huawei.com>
2024-05-28 03:39:06 +0000

    update interfaces/kits/js/src/mod_fs/properties/copy_listener/trans_listener.cpp.
    Signed-off-by: lixiang <lixiang493@huawei.com>

--- interfaces/kits/js/src/mod_fs/properties/copy_listener/trans_listener.cpp
+++ interfaces/kits/js/src/mod_fs/properties/copy_listener/trans_listener.cpp
@@ -90,9 +90,9 @@ NError TransListener::CopyFileFromSoftBus(const std::string &srcUri, const std::
         }
         auto it = softbusErr2ErrCodeTable.find(transListener->copyEvent_.errorCode);
         if (it != softbusErr2ErrCodeTable.end()) {
-            return NError(it->second);
+            return NError(EIO);
         }
-        return NError(EIO);
+        return NError(it->second);
     }
     if (info.authority == FILE_MANAGER_AUTHORITY || info.authority == MEDIA_AUTHORITY) {
         HILOGW("Public or media path not copy");
