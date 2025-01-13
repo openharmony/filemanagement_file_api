2fea731fc31976fdf8456fa3e92fe3ed1deb82f2
zhongning5 <zhongning5@huawei.com>
2025-01-13 13:56:23 +0800

    使能atomicFile
    Signed-off-by: zhongning5 <zhongning5@huawei.com>

--- interfaces/kits/ts/streamrw/src/streamrw.ts
+++ interfaces/kits/ts/streamrw/src/streamrw.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Copyright (c) 2024-2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -139,6 +139,10 @@ class WriteStream extends stream.Writable {
 
     close(): void {
         this.stream?.close();
+    }
+
+    closeSync(): void {
+        this.stream?.closeSync();
     }
 
     doInitialize(callback: Function): void {
