920ee0e5db9e7f7dbbceb6387cf2d7db943c4a32
tianp <tianpeng35@huawei.com>
2025-03-06 20:53:00 +0800

    move ANI层
    Signed-off-by: tianp <tianpeng35@huawei.com>

---
+++ interfaces/kits/js/src/mod_fs/properties/ani/move_ani.cpp
@@ -0,0 +1,57 @@
+/*
+* Copyright (c) 2025 Huawei Device Co., Ltd.
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#include "move_ani.h"
+
+#include <string>
+#include "filemgmt_libhilog.h"
+#include "move_core.h"
+#include "type_converter.h"
+
+namespace OHOS {
+namespace FileManagement {
+namespace ModuleFileIO {
+namespace ANI {
+
+void MoveAni::MoveFileSync(ani_env *env, [[maybe_unused]] ani_class clazz,
+                           ani_string src, ani_string dest, ani_object mode)
+{
+    auto [succSrc, srcPath] = ANI::TypeConverter::ToUTF8StringPath(env, src);
+    if (!succSrc) {
+        HILOGE("Invalid src");
+        return;
+    }
+    auto [succDest, destPath] = ANI::TypeConverter::ToUTF8StringPath(env, dest);
+    if (!succDest) {
+        HILOGE("Invalid dest");
+        return;
+    }
+    auto [succMode, modeOp] = ANI::TypeConverter::ToOptionalInt32(env, mode);
+    if (!succMode) {
+        HILOGE("Invalid mode");
+        return;
+    }
+    auto ret = MoveCore::DoMove(srcPath, destPath, modeOp);
+    if (!ret.IsSuccess()) {
+        HILOGE("move feild");
+        return;
+    }
+    HILOGE("move success");
+        return;
+}
+} // ANI
+} // namespace ModuleFileIO
+} // namespace FileManagement
+} // namespace OHOS
