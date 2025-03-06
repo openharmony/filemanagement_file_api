e39f8f4d329b62efb2a3e63c3aea2128300025ad
兔叽和猪扒 <zhujiating2@huawei.com>
2025-03-06 18:19:33 +0800

    rmdir ani化
    Signed-off-by: 兔叽和猪扒 <zhujiating2@huawei.com>

---
+++ interfaces/kits/js/src/mod_fs/properties/ani/rmdir_ani.cpp
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <string>
+#include "rmdir_ani.h"
+#include "filemgmt_libhilog.h"
+#include "type_converter.h"
+#include "rmdir_core.h"
+
+namespace OHOS {
+namespace FileManagement {
+namespace ModuleFileIO {
+namespace ANI {
+using namespace std;
+
+void RmdirAni::rmdirSync(ani_env *env, [[maybe_unused]] ani_class clazz, ani_string path) 
+{
+    auto [succPath, pathStr] = TypeConverter::ToUTF8StringPath(env, path);
+    if (!succPath) {
+        HILOGE("Invalid path");
+        return ;
+    }
+    RmdirentCore::DoRmdirent(pathStr);
+    HILOGE("rmdirSync pathStr: %s", pathStr.c_str());
+}
+
+} // ANI
+} // namespace ModuleFileIO
+} // namespace FileManagement
+} // namespace OHOS
