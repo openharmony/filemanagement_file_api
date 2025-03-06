b961a8386c358a3792d40f0662d9032b08f20775
周鑫 <zhouxin176@huawei.com>
2025-03-06 21:28:16 +0800

    securitylabel桥阶层
    Signed-off-by: 周鑫 <zhouxin176@huawei.com>

---
+++ interfaces/kits/js/src/mod_securitylabel/ani/securitylabel_ani.cpp
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "filemgmt_libhilog.h"
+#include "securitylabel_ani.h"
+#include "securitylabel_core.h"
+#include "type_converter.h"
+
+namespace OHOS {
+namespace FileManagement {
+namespace ModuleFileIo {
+namespace ANI {
+
+using namespace std;
+using namespace OHOS::FileManagement::ModuleFileIO;
+using namespace OHOS::FileManagement::ModuleFileIO::ANI;
+using namespace OHOS::FileManagement::ModuleSecurityLabel;
+
+ani_int SecurityLabelAni::SetSecurityLabelSync(ani_env *env, [[maybe_unused]] ani_class clazz, ani_string path, ani_string level)
+{
+    auto [succPath, srcPath] = TypeConverter::ToUTF8StringPath(env, path);
+    if (!succPath) {
+        HILOGE("Invalid path");
+        return EINVAL;
+    }
+
+    auto [succLevel, dataLevel] = TypeConverter::ToUTF8StringPath(env, level);
+    if (!succLevel) {
+        HILOGE("Invalid dataLevel");
+        return EINVAL;
+    }
+
+    // 核心逻辑
+    auto ret = DoSetSecurityLabel(srcPath, dataLevel);
+    if (!ret.IsSuccess()) {
+        // TODO：错误处理
+        HILOGE("file %s set securitylabel failed", srcPath.c_str());
+        return -1;
+    }
+
+    return 0;
+}
+
+} // ANI
+} // namespcae ModuleFileIo
+} // namespcae FileManagement
+} // namespcae OHOS
