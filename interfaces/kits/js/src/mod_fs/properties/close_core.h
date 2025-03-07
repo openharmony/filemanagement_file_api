31b2f4a1168fe457dcea4ffefaa31f54783cf256
WangKe <wangke185@huawei.com>
2025-03-07 23:15:43 +0800

    add close_core
    Signed-off-by: WangKe <wangke185@huawei.com>

---
+++ interfaces/kits/js/src/mod_fs/properties/close_core.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef INTERFACES_KITS_JS_SRC_MOD_FS_PROPERTIES_CLOSE_CORE_H
+#define INTERFACES_KITS_JS_SRC_MOD_FS_PROPERTIES_CLOSE_CORE_H
+
+#include "filemgmt_libfs.h"
+#include "fs_utils.h"
+#include "fs_result.h"
+
+namespace OHOS::FileManagement::ModuleFileIO {
+
+class CloseCore final {
+public:
+    static FsResult<void> DoClose(const int32_t &fd);
+};
+
+const std::string PROCEDURE_CLOSE_NAME = "FileIOClose";
+} // namespace OHOS::FileManagement::ModuleFileIO
+
+#endif // INTERFACES_KITS_JS_SRC_MOD_FS_PROPERTIES_CLOSE_CORE_H
