0f269b63fcd767c7776326b64b09a268890f4cd3
姜小林 <jiangxiaolin11@huawei.com>
2025-05-25 18:10:11 +0800

    部件独立化编译调整，调整BUILD.gn和bundle.json配置文件，调整头文件的引入方式
    Change-Id: Ife1def179f665b472b793170a7bed7422edad66e
    Signed-off-by: 姜小林 <jiangxiaolin11@huawei.com>

--- interfaces/kits/js/src/mod_fs/properties/watcher.cpp
+++ interfaces/kits/js/src/mod_fs/properties/watcher.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Copyright (c) 2023-2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -21,10 +21,11 @@
 #include <tuple>
 #include <unistd.h>
 
+#include "class_watcher/watcher_entity.h"
+#include "class_watcher/watcher_n_exporter.h"
 #include "file_utils.h"
 #include "filemgmt_libhilog.h"
-#include "../class_watcher/watcher_entity.h"
-#include "../class_watcher/watcher_n_exporter.h"
+
 namespace OHOS::FileManagement::ModuleFileIO {
 using namespace std;
 using namespace OHOS::FileManagement::LibN;
