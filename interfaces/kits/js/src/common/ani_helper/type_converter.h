3de6e33d38f9dec3726ba570fc47c0d8f7f06eac
姜小林 <jiangxiaolin11@huawei.com>
2025-06-05 11:50:36 +0800

    增加ani_helper相关通用工具类
    Change-Id: Ic4b998ffeb70b9c89615cf315fde42778b865349
    Signed-off-by: 姜小林 <jiangxiaolin11@huawei.com>

---
+++ interfaces/kits/js/src/common/ani_helper/type_converter.h
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef FILEMANAGEMENT_ANI_TYPE_CONVERTER_H
+#define FILEMANAGEMENT_ANI_TYPE_CONVERTER_H
+
+#include <string>
+#include <tuple>
+
+#include <ani.h>
+
+#include "fs_array_buffer.h"
+#include "fs_utils.h"
+
+namespace OHOS::FileManagement::ModuleFileIO::ANI {
+inline const std::string EMPTY_STRING = "";
+
+class TypeConverter {
+public:
+    static std::tuple<bool, std::string> ToUTF8String(ani_env *env, const ani_string &path);
+    static std::tuple<bool, std::optional<int32_t>> ToOptionalInt32(ani_env *env, const ani_object &value);
+    static std::tuple<bool, std::optional<int64_t>> ToOptionalInt64(ani_env *env, const ani_object &value);
+    static std::tuple<bool, ani_arraybuffer> ToAniArrayBuffer(ani_env *env, void *buffer, size_t length);
+    static std::tuple<bool, ani_string> ToAniString(ani_env *env, std::string str);
+    static std::tuple<bool, ani_string> ToAniString(ani_env *env, std::string str, size_t size);
+    static std::tuple<bool, ani_string> ToAniString(ani_env *env, const char *str);
+    static std::tuple<bool, std::optional<int32_t>> EnumToInt32(ani_env *env, const ani_enum_item &enumOp);
+    static std::tuple<bool, FileInfo> ToFileInfo(ani_env *env, const ani_object &pathOrFd);
+    static std::tuple<bool, ArrayBuffer> ToArrayBuffer(ani_env *env, ani_arraybuffer &buffer);
+    static std::tuple<bool, ani_array_ref> ToAniStringList(
+        ani_env *env, const std::string strList[], const uint32_t length);
+};
+
+} // namespace OHOS::FileManagement::ModuleFileIO::ANI
+
+#endif // FILEMANAGEMENT_ANI_TYPE_CONVERTER_H
