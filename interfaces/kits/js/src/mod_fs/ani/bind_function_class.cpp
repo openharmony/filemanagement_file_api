e491848b4ccac2586bd1fea9f4f944fc07610c1a
liyuke <liyuke7@huawei.com>
2025-06-06 15:38:15 +0800

    mkdir接口回合master
    Signed-off-by: liyuke <liyuke7@huawei.com>
    Change-Id: Ifb0953568fffb2562eb6a8ea3a9745f85494d931

---
+++ interfaces/kits/js/src/mod_fs/ani/bind_function_class.cpp
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <array>
+
+#include <ani.h>
+
+#include "ani_signature.h"
+#include "bind_function.h"
+#include "mkdir_ani.h"
+
+using namespace OHOS::FileManagement::ModuleFileIO::ANI;
+using namespace OHOS::FileManagement::ModuleFileIO::ANI::AniSignature;
+
+const static string mkdirCtorSig0 = Builder::BuildSignatureDescriptor({ BuiltInTypes::stringType });
+const static string mkdirCtorSig1 =
+    Builder::BuildSignatureDescriptor({ BuiltInTypes::stringType, BasicTypes::booleanType });
+
+static ani_status BindStaticMethods(ani_env *env)
+{
+    auto classDesc = Impl::FileIoImpl::classDesc.c_str();
+
+    std::array methods = {
+        ani_native_function { "mkdirSync", mkdirCtorSig0.c_str(), reinterpret_cast<void *>(MkdirkAni::MkdirSync0) },
+        ani_native_function { "mkdirSync", mkdirCtorSig1.c_str(), reinterpret_cast<void *>(MkdirkAni::MkdirSync1) },
+    };
+    return BindClass(env, classDesc, methods);
+}
+
+static ani_status DoBindMethods(ani_env *env)
+{
+    ani_status status;
+    if ((status = BindStaticMethods(env)) != ANI_OK) {
+        HILOGE("Cannot bind native static methods for BindStaticMethods!");
+        return status;
+    };
+
+    return ANI_OK;
+}
+
+ANI_EXPORT ani_status ANI_Constructor(ani_vm *vm, uint32_t *result)
+{
+    if (vm == nullptr) {
+        HILOGE("Invalid parameter vm");
+        return ANI_INVALID_ARGS;
+    }
+
+    if (result == nullptr) {
+        HILOGE("Invalid parameter result");
+        return ANI_INVALID_ARGS;
+    }
+
+    ani_env *env;
+    ani_status status = vm->GetEnv(ANI_VERSION_1, &env);
+    if (status != ANI_OK) {
+        HILOGE("Invalid ani version!");
+        return ANI_INVALID_VERSION;
+    }
+
+    status = DoBindMethods(env);
+    if (status != ANI_OK) {
+        return status;
+    }
+
+    *result = ANI_VERSION_1;
+    return ANI_OK;
+}
---
+++ interfaces/kits/js/src/mod_fs/properties/mkdir_core.cpp
@@ -0,0 +1,112 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "mkdir_core.h"
+
+#include <cstring>
+#include <ctime>
+#include <iostream>
+#include <memory>
+#include <sstream>
+#include <unistd.h>
+
+#include "filemgmt_libhilog.h"
+
+#if !defined(WIN_PLATFORM) && !defined(IOS_PLATFORM)
+#include "rust_file.h"
+#endif
+
+#ifdef FILE_API_TRACE
+#include "hitrace_meter.h"
+#endif
+
+namespace OHOS {
+namespace FileManagement {
+namespace ModuleFileIO {
+using namespace std;
+
+static int UvAccess(const string &path, int mode)
+{
+    std::unique_ptr<uv_fs_t, decltype(FsUtils::FsReqCleanup) *> access_req = { new uv_fs_t, FsUtils::FsReqCleanup };
+    if (!access_req) {
+        HILOGE("Failed to request heap memory.");
+        return ENOMEM;
+    }
+    return uv_fs_access(nullptr, access_req.get(), path.c_str(), mode, nullptr);
+}
+
+static int MkdirCore(const string &path)
+{
+    std::unique_ptr<uv_fs_t, decltype(FsUtils::FsReqCleanup) *> mkdir_req = { new uv_fs_t, FsUtils::FsReqCleanup };
+    if (!mkdir_req) {
+        HILOGE("Failed to request heap memory.");
+        return ENOMEM;
+    }
+    return uv_fs_mkdir(nullptr, mkdir_req.get(), path.c_str(), DIR_DEFAULT_PERM, nullptr);
+}
+
+static int32_t MkdirExec(const string &path, bool recursion, bool hasOption)
+{
+#if !defined(WIN_PLATFORM) && !defined(IOS_PLATFORM)
+    if (hasOption) {
+        int ret = UvAccess(path, 0);
+        if (ret == ERRNO_NOERR) {
+            HILOGD("The path already exists");
+            return EEXIST;
+        }
+        if (ret != -ENOENT) {
+            HILOGE("Failed to check for illegal path or request for heap memory");
+            return ret;
+        }
+        if (::Mkdirs(path.c_str(), static_cast<MakeDirectionMode>(recursion)) < 0) {
+            HILOGD("Failed to create directories, error: %{public}d", errno);
+            return errno;
+        }
+        ret = UvAccess(path, 0);
+        if (ret) {
+            HILOGE("Failed to verify the result of Mkdirs function");
+            return ret;
+        }
+        return ERRNO_NOERR;
+    }
+#endif
+    int ret = MkdirCore(path);
+    if (ret) {
+        HILOGD("Failed to create directory");
+        return ret;
+    }
+    return ERRNO_NOERR;
+}
+
+FsResult<void> MkdirCore::DoMkdir(const std::string &path, std::optional<bool> recursion)
+{
+    bool hasOption = false;
+    bool mkdirRecursion = false;
+#if !defined(WIN_PLATFORM) && !defined(IOS_PLATFORM)
+    hasOption = recursion.has_value();
+    if (hasOption) {
+        mkdirRecursion = recursion.value();
+    }
+#endif
+    auto err = MkdirExec(path, mkdirRecursion, hasOption);
+    if (err) {
+        return FsResult<void>::Error(err);
+    }
+    return FsResult<void>::Success();
+}
+
+} // namespace ModuleFileIO
+} // namespace FileManagement
+} // namespace OHOS
