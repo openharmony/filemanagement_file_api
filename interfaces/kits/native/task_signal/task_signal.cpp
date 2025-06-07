7ecd823bb1440d568a0ad967f65fcd4c9ca1550d
g00613291 <gengzhuangzhuang@huawei.com>
2025-06-07 14:25:09 +0800

    fix cancel
    Signed-off-by: g00613291 <gengzhuangzhuang@huawei.com>

--- interfaces/kits/native/task_signal/task_signal.cpp
+++ interfaces/kits/native/task_signal/task_signal.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -27,7 +27,7 @@ int32_t TaskSignal::Cancel()
 {
     HILOGD("TaskSignal Cancel in.");
     if (dfsCopyTask_.load()) {
-        auto ret = Storage::DistributedFile::FileCopyManager::GetInstance()->Cancel(srcUri_, dstUri_);
+        auto ret = Storage::DistributedFile::FileCopyManager::GetInstance()->Cancel(srcUri_, dstUri_, true);
         if (ret != 0) {
             HILOGE("Cancel failed, ret = %{public}d", ret);
             return ret;
@@ -38,7 +38,7 @@ int32_t TaskSignal::Cancel()
     if (remoteTask_.load()) {
         int32_t ret = 0;
         if (sessionName_.empty()) {
-            ret = Storage::DistributedFile::FileCopyManager::GetInstance()->Cancel(srcUri_, dstUri_);
+            ret = Storage::DistributedFile::FileCopyManager::GetInstance()->Cancel(srcUri_, dstUri_, true);
         } else {
             ret = Storage::DistributedFile::DistributedFileDaemonManager::GetInstance().
                 CancelCopyTask(sessionName_);
