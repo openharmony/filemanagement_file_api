9f5a22b0bd3a2c5d3aaf5e618282fead483bebaa
tianp <tianpeng35@huawei.com>
2025-06-17 16:24:11 +0800

    修改安全函数
    Signed-off-by: tianp <tianpeng35@huawei.com>

--- interfaces/test/unittest/js/mod_fs/class_stat/fs_stat_mock_test.cpp
+++ interfaces/test/unittest/js/mod_fs/class_stat/fs_stat_mock_test.cpp
@@ -13,12 +13,13 @@
  * limitations under the License.
  */
 
-#include "fs_stat.h"
-#include "../properties/mock/system_mock.h"
-#include "fs_stat_entity.h"
-
-#include <gtest/gtest.h>
 #include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include "fs_stat_entity.h"
+#include "fs_stat.h"
+#include "securec.h"
+#include "system_mock.h"
 
 namespace OHOS::FileManagement::ModuleFileIO::Test {
 using namespace testing;
@@ -74,8 +75,10 @@ HWTEST_F(FsStatMockTest, FsStatMockTest_GetLocation_001, testing::ext::TestSize.
     statEntity = std::make_unique<StatEntity>();
     statEntity->fileInfo_ = std::make_unique<FileInfo>();
     statEntity->fileInfo_->isPath = true;
-    statEntity->fileInfo_->path = std::make_unique<char[]>(100);
-    strncpy(stat->fileInfo_->path.get(), "/test/path", 99);
+    int length = 100;
+    string testPath = "/test/path";
+    statEntity->fileInfo_->path = std::make_unique<char[]>(length);
+    strncpy_s(statEntity->fileInfo_->path.get(), length, testPath.c_str(), testPath.size());
     statEntity->fileInfo_->path.get()[99] = '\0';
     fsStat = std::make_unique<FsStat>(std::move(statEntity));
 
