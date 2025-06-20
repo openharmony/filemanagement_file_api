438894a9160a51eb233a56b3a4d4d811f697b2bf
tianp <tianpeng35@huawei.com>
2025-06-20 12:13:44 +0800

    open,fileTDD用例
    Signed-off-by: tianp <tianpeng35@huawei.com>
    Change-Id: Ib89a14404e229cc3c60cb2729ee420afb4cbdece

--- interfaces/test/unittest/js/mod_fs/class_file/fs_file_test.cpp
+++ interfaces/test/unittest/js/mod_fs/class_file/fs_file_test.cpp
@@ -18,6 +18,7 @@
 
 #include <gtest/gtest.h>
 
+
 namespace OHOS::FileManagement::ModuleFileIO::Test {
 using namespace testing;
 using namespace testing::ext;
@@ -256,6 +257,7 @@ HWTEST_F(FsFileTest, FsFileTest_GetParent_011, testing::ext::TestSize.Level1)
 
     GTEST_LOG_(INFO) << "FsFileTest-end FsFileTest_GetParent_011";
 }
+
 
 /**
  * @tc.name: FsFileTest_GetPath_012
