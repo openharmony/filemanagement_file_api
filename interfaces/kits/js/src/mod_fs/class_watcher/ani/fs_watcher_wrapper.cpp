4916480af0901b3ea8ae498e828db5a66422021e
tianp <tianpeng35@huawei.com>
2025-07-26 14:31:38 +0800

    watcher功能
    Signed-off-by: tianp <tianpeng35@huawei.com>
    Change-Id: I5c6e9f85569b14ef50cb271864b63b6d451bb980

---
+++ interfaces/kits/js/src/mod_fs/class_watcher/ani/fs_watcher_wrapper.cpp
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "fs_watcher_wrapper.h"
+
+#include "ani_signature.h"
+#include "filemgmt_libhilog.h"
+
+namespace OHOS {
+namespace FileManagement {
+namespace ModuleFileIO {
+namespace ANI {
+using namespace std;
+using namespace OHOS::FileManagement::ModuleFileIO;
+using namespace OHOS::FileManagement::ModuleFileIO::ANI::AniSignature;
+
+FsWatcher *FsWatcherWrapper::Unwrap(ani_env *env, ani_object object)
+{
+    ani_long nativePtr;
+    auto ret = env->Object_GetFieldByName_Long(object, "nativePtr", &nativePtr);
+    if (ret != ANI_OK) {
+        HILOGE("Unwrap fsWatcher err: %{private}d", ret);
+        return nullptr;
+    }
+    uintptr_t ptrValue = static_cast<uintptr_t>(nativePtr);
+    FsWatcher *watcher = reinterpret_cast<FsWatcher *>(ptrValue);
+    return watcher;
+}
+
+ani_object FsWatcherWrapper::Wrap(ani_env *env, const FsWatcher *watcher)
+{
+    if (watcher == nullptr) {
+        HILOGE("FsWatcher pointer is null!");
+        return nullptr;
+    }
+    auto classDesc = FS::WatcherInner::classDesc.c_str();
+    ani_class cls;
+    if (ANI_OK != env->FindClass(classDesc, &cls)) {
+        HILOGE("Cannot find class %s", classDesc);
+        return nullptr;
+    }
+    auto ctorDesc = FS::WatcherInner::ctorDesc.c_str();
+    auto ctorSig = FS::WatcherInner::ctorSig.c_str();
+    ani_method ctor;
+    if (ANI_OK != env->Class_FindMethod(cls, ctorDesc, ctorSig, &ctor)) {
+        HILOGE("Cannot find constructor method for class %s", classDesc);
+        return nullptr;
+    }
+    ani_long ptr = static_cast<ani_long>(reinterpret_cast<std::uintptr_t>(watcher));
+    ani_object obj;
+    if (ANI_OK != env->Object_New(cls, ctor, &obj, ptr)) {
+        HILOGE("New %s obj Failed!", classDesc);
+        return nullptr;
+    }
+    return obj;
+}
+
+} // namespace ANI
+} // namespace ModuleFileIO
+} // namespace FileManagement
+} // namespace OHOS
---
+++ interfaces/kits/js/src/mod_fs/properties/watcher_core.cpp
@@ -0,0 +1,108 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "watcher_core.h"
+
+#include <cstring>
+#include <fcntl.h>
+#include <tuple>
+
+#include "file_utils.h"
+#include "filemgmt_libhilog.h"
+#include "fs_file_watcher.h"
+
+namespace OHOS::FileManagement::ModuleFileIO {
+using namespace std;
+
+static FsResult<FsWatcher *> InstantiateWatcher()
+{
+    if (FsFileWatcher::GetInstance().GetNotifyId() < 0 && !FsFileWatcher::GetInstance().InitNotify()) {
+        HILOGE("Failed to get notifyId or initnotify fail");
+        return FsResult<FsWatcher *>::Error(errno);
+    }
+    FsResult<FsWatcher *> result = FsWatcher::Constructor();
+    if (!result.IsSuccess()) {
+        HILOGE("Failed to instantiate watcher");
+        return FsResult<FsWatcher *>::Error(EIO);
+    }
+    return result;
+}
+
+shared_ptr<WatcherInfo> ToWatcherInfo(
+    const string &path, const int32_t events, shared_ptr<IWatcherCallback> callback, int32_t &errCode)
+{
+    if (events <= 0 || !FsFileWatcher::GetInstance().CheckEventValid(events)) {
+        HILOGE("Failed to get watcher event.");
+        errCode = EINVAL;
+        return nullptr;
+    }
+
+    if (!callback) {
+        HILOGE("Failed to get callback");
+        errCode = EINVAL;
+        return nullptr;
+    }
+
+    auto info = CreateSharedPtr<WatcherInfo>(callback);
+    if (info == nullptr) {
+        HILOGE("Failed to request heap memory.");
+        errCode = ENOMEM;
+        return nullptr;
+    }
+
+    info->events = static_cast<uint32_t>(events);
+    info->fileName = move(path);
+    return info;
+}
+
+FsResult<FsWatcher *> WatcherCore::DoCreateWatcher(
+    const string &path, const int32_t events, shared_ptr<IWatcherCallback> callback)
+{
+    int errCode = 0;
+    auto info = ToWatcherInfo(path, events, callback, errCode);
+    if (errCode != 0) {
+        HILOGE("Failed to parse param");
+        return FsResult<FsWatcher *>::Error(errCode);
+    }
+
+    auto result = InstantiateWatcher();
+    if (!result.IsSuccess()) {
+        return result;
+    }
+
+    const FsWatcher *objWatcher = result.GetData().value();
+    if (!objWatcher) {
+        HILOGE("Failed to get fsWatcher");
+        return FsResult<FsWatcher *>::Error(EIO);
+    }
+
+    auto *watchEntity = objWatcher->GetWatchEntity();
+    if (!watchEntity) {
+        HILOGE("Failed to get watchEntity.");
+        delete objWatcher;
+        objWatcher = nullptr;
+        return FsResult<FsWatcher *>::Error(EIO);
+    }
+
+    watchEntity->data_ = info;
+
+    bool ret = FsFileWatcher::GetInstance().AddWatcherInfo(info);
+    if (!ret) {
+        HILOGE("Failed to add watcher info.");
+        return FsResult<FsWatcher *>::Error(EINVAL);
+    }
+    return result;
+}
+} // namespace OHOS::FileManagement::ModuleFileIO
