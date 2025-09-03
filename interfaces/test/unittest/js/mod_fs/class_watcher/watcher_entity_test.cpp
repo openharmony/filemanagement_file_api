bc5301a282f55240f24943eb7c6d8f895ff3025e
tianp <tianpeng35@huawei.com>
2025-09-03 17:37:33 +0800

    watcher_tdd add
    Signed-off-by: tianp <tianpeng35@huawei.com>
    Change-Id: I668e7fa35562a571434e69bef0199cf17cf36389

--- interfaces/test/unittest/js/mod_fs/class_watcher/watcher_entity_test.cpp
+++ interfaces/test/unittest/js/mod_fs/class_watcher/watcher_entity_test.cpp
@@ -18,6 +18,8 @@
 #include <gtest/gtest.h>
 #include <thread>
 
+#include "unistd_mock.h"
+
 namespace OHOS::FileManagement::ModuleFileIO::Test {
 
 class WatcherEntityTest : public testing::Test {
@@ -104,6 +106,92 @@ HWTEST_F(WatcherEntityTest, WatcherEntityTest_CloseNotifyFd_003, testing::ext::T
     EXPECT_NE(ret, 0);
 
     GTEST_LOG_(INFO) << "WatcherEntityTest-end WatcherEntityTest_CloseNotifyFd_003";
+}
+
+/**
+ * @tc.name: WatcherEntityTest_ReadNotifyEvent_001
+ * @tc.desc: Test function of WatcherEntityTest::ReadNotifyEvent interface for SUCCESS when read valid event data.
+ * @tc.size: MEDIUM
+ * @tc.type: FUNC
+ * @tc.level Level 1
+ */
+HWTEST_F(WatcherEntityTest, WatcherEntityTest_ReadNotifyEvent_001, testing::ext::TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "WatcherEntityTest-begin WatcherEntityTest_ReadNotifyEvent_001";
+
+    UnistdMock::EnableMock();
+    auto &watcher = FileWatcher::GetInstance();
+    int32_t len = static_cast<int32_t>(sizeof(struct inotify_event));
+    WatcherCallback callback = nullptr;
+
+    auto unistdMock = UnistdMock::GetMock();
+    EXPECT_CALL(*unistdMock, read(testing::_, testing::_, testing::_)).Times(1).WillOnce(testing::Return(len));
+
+    watcher.ReadNotifyEvent(callback);
+    testing::Mock::VerifyAndClearExpectations(unistdMock.get());
+    UnistdMock::DisableMock();
+
+    GTEST_LOG_(INFO) << "WatcherEntityTest-end WatcherEntityTest_ReadNotifyEvent_001";
+}
+
+/**
+ * @tc.name: WatcherEntityTest_ReadNotifyEvent_002
+ * @tc.desc: Test first if branch - remaining data < sizeof(inotify_event)
+ * @tc.size: MEDIUM
+ * @tc.type: FUNC
+ * @tc.level Level 1
+ */
+HWTEST_F(WatcherEntityTest, WatcherEntityTest_ReadNotifyEvent_002, testing::ext::TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "WatcherEntityTest-begin WatcherEntityTest_ReadNotifyEvent_002";
+
+    UnistdMock::EnableMock();
+    auto &watcher = FileWatcher::GetInstance();
+    int32_t eventSize = static_cast<int32_t>(sizeof(struct inotify_event));
+    int32_t partialSize = eventSize - 4;
+    WatcherCallback callback = nullptr;
+
+    auto unistdMock = UnistdMock::GetMock();
+    EXPECT_CALL(*unistdMock, read(testing::_, testing::_, testing::_)).Times(1).WillOnce(testing::Return(partialSize));
+
+    watcher.ReadNotifyEvent(callback);
+    testing::Mock::VerifyAndClearExpectations(unistdMock.get());
+    UnistdMock::DisableMock();
+
+    GTEST_LOG_(INFO) << "WatcherEntityTest-end WatcherEntityTest_ReadNotifyEvent_002";
+}
+
+/**
+ * @tc.name: WatcherEntityTest_ReadNotifyEvent_003
+ * @tc.desc: Test second if branch - event->len exceeds available buffer
+ * @tc.size: SMALL
+ * @tc.type: FUNC
+ * @tc.level Level 1
+ */
+HWTEST_F(WatcherEntityTest, WatcherEntityTest_ReadNotifyEvent_003, testing::ext::TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "WatcherEntityTest-begin WatcherEntityTest_ReadNotifyEvent_003";
+
+    UnistdMock::EnableMock();
+    auto &watcher = FileWatcher::GetInstance();
+    int32_t eventSize = static_cast<int32_t>(sizeof(struct inotify_event));
+    uint32_t nameLen = 20;
+    int32_t bufferSize = eventSize + 10;
+    WatcherCallback callback = nullptr;
+
+    char buf[BUF_SIZE] = {0};
+    struct inotify_event* mockEvent = reinterpret_cast<inotify_event*>(buf);
+    mockEvent->wd = 1;
+    mockEvent->len = nameLen;
+
+    auto unistdMock = UnistdMock::GetMock();
+    EXPECT_CALL(*unistdMock, read(testing::_, testing::_, testing::_)).Times(1).WillOnce(testing::Return(bufferSize));
+
+    watcher.ReadNotifyEvent(callback);
+    testing::Mock::VerifyAndClearExpectations(unistdMock.get());
+    UnistdMock::DisableMock();
+
+    GTEST_LOG_(INFO) << "WatcherEntityTest-end WatcherEntityTest_ReadNotifyEvent_003";
 }
 
 } // namespace OHOS::FileManagement::ModuleFileIO::Test
