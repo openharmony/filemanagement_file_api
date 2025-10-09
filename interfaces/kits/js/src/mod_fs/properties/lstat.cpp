be0f1bf17bf6fd0562861ec994e03189b2faa8fd
gudongyang <gudongyang@h-partners.com>
2025-10-09 11:11:17 +0800

    修改评审意见
    Signed-off-by: gudongyang <gudongyang@h-partners.com>

--- interfaces/kits/js/src/mod_fs/properties/lstat.cpp
+++ interfaces/kits/js/src/mod_fs/properties/lstat.cpp
@@ -63,7 +63,7 @@ napi_value Lstat::Sync(napi_env env, napi_callback_info info)
         return nullptr;
     }
     string pathStr(pathPtr.get());
-    auto [succ, path]  = ParsePath(pathStr);
+    auto [succ, path]  = ParsePath(env, pathStr);
     if (!succ) {
         return nullptr;
     }
@@ -120,7 +120,7 @@ napi_value Lstat::Async(napi_env env, napi_callback_info info)
     }
 
     string pathStr(tmp.get());
-    auto [succ, path]  = ParsePath(pathStr);
+    auto [succ, path]  = ParsePath(env, pathStr);
     if (!succ) {
         return nullptr;
     }
