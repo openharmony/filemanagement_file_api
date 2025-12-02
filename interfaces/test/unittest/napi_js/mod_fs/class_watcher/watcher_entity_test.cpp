8e9d284aa2e0db3a1d90a881691ff5b4be85fd6b
sutisiya <zhouxin176@h-partners.com>
2025-12-02 09:56:30 +0800

    multi test
    Co-authored-by: sutisiya<zhouxin176@huawei.com>

--- interfaces/test/unittest/napi_js/mod_fs/class_watcher/watcher_entity_test.cpp
+++ interfaces/test/unittest/napi_js/mod_fs/class_watcher/watcher_entity_test.cpp
@@ -15,12 +15,18 @@
 
 #include "watcher_entity.h"
 
+#include <atomic>
+#include <chrono>
+#include <thread>
+#include <vector>
+
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 #include <sys/prctl.h>
 
 #include "eventfd_mock.h"
 #include "inotify_mock.h"
+#include "poll_mock.h"
 #include "unistd_mock.h"
 
 namespace OHOS::FileManagement::ModuleFileIO::Test {
@@ -49,6 +55,7 @@ void WatcherEntityTest::SetUp(void)
     GTEST_LOG_(INFO) << "SetUp";
     EventfdMock::EnableMock();
     InotifyMock::EnableMock();
+    PollMock::EnableMock();
     UnistdMock::EnableMock();
     errno = 0;
 }
@@ -58,6 +65,7 @@ void WatcherEntityTest::TearDown(void)
     // Reset all mocks
     EventfdMock::DisableMock();
     InotifyMock::DisableMock();
+    PollMock::DisableMock();
     UnistdMock::DisableMock();
     // Reset FileWatcher stat
     auto &watcher = FileWatcher::GetInstance();
@@ -75,6 +83,7 @@ inline const int32_t INITIALIZED_NOTIFYFD = 1;
 inline const int32_t UNINITIALIZED_NOTIFYFD = -1;
 inline const int32_t INITIALIZED_EVENTFD = 1;
 inline const int32_t UNINITIALIZED_EVENTFD = -1;
+inline const int32_t TOTAL_THREADS = 20;
 
 /**
  * @tc.name: WatcherEntityTest_CloseNotifyFd_001
@@ -349,6 +358,189 @@ HWTEST_F(WatcherEntityTest, WatcherEntityTest_CloseNotifyFdLocked_002, testing::
     EXPECT_EQ(ret, 0);
 
     GTEST_LOG_(INFO) << "WatcherEntityTest-end WatcherEntityTest_CloseNotifyFdLocked_002";
+}
+
+/**
+ * @tc.name: WatcherEntityTest_CloseNotifyFdLocked_003
+ * @tc.desc: Test function of WatcherEntityTest::CloseNotifyFdLocked interface for multi.
+ * @tc.size: MEDIUM
+ * @tc.type: FUNC
+ * @tc.level Level 1
+ */
+HWTEST_F(WatcherEntityTest, WatcherEntityTest_CloseNotifyFdLocked_003, testing::ext::TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "WatcherEntityTest-begin WatcherEntityTest_CloseNotifyFdLocked_003";
+
+    auto &watcher = FileWatcher::GetInstance();
+    watcher.reading_ = true;
+    watcher.notifyFd_ = 1;
+    watcher.eventFd_ = 1;
+    std::vector<std::thread> threads;
+    threads.reserve(TOTAL_THREADS);
+
+    for (int i = 0; i < TOTAL_THREADS; ++i) {
+        threads.emplace_back([&watcher]() {
+            int result = watcher.CloseNotifyFdLocked();
+            EXPECT_EQ(result, 0);
+        });
+    }
+
+    for (auto& t : threads) {
+        t.join();
+    }
+
+    EXPECT_TRUE(watcher.closed_);
+    EXPECT_EQ(watcher.notifyFd_, 1);
+    EXPECT_EQ(watcher.eventFd_, 1);
+
+    GTEST_LOG_(INFO) << "WatcherEntityTest-end WatcherEntityTest_CloseNotifyFdLocked_003";
+}
+
+/**
+ * @tc.name: WatcherEntityTest_CloseNotifyFdLocked_004
+ * @tc.desc: Test function of WatcherEntityTest::CloseNotifyFdLocked interface for multi.
+ * @tc.size: MEDIUM
+ * @tc.type: FUNC
+ * @tc.level Level 1
+ */
+HWTEST_F(WatcherEntityTest, WatcherEntityTest_CloseNotifyFdLocked_004, testing::ext::TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "WatcherEntityTest-begin WatcherEntityTest_CloseNotifyFdLocked_004";
+
+    auto &watcher = FileWatcher::GetInstance();
+    watcher.run_ = false;
+    watcher.reading_ = false;
+    watcher.notifyFd_ = 1;
+    watcher.eventFd_ = 1;
+    std::vector<std::thread> threads;
+    threads.reserve(TOTAL_THREADS);
+
+    for (int i = 0; i < TOTAL_THREADS; ++i) {
+        threads.emplace_back([&watcher]() {
+            int result = watcher.CloseNotifyFdLocked();
+            EXPECT_EQ(result, 0);
+        });
+    }
+
+    auto unistdMock = UnistdMock::GetMock();
+    EXPECT_CALL(*unistdMock, close(testing::_)).Times(0);
+
+    for (auto& t : threads) {
+        t.join();
+    }
+
+    testing::Mock::VerifyAndClearExpectations(unistdMock.get());
+    EXPECT_FALSE(watcher.closed_);
+    EXPECT_EQ(watcher.notifyFd_, 1);
+    EXPECT_EQ(watcher.eventFd_, 1);
+
+    GTEST_LOG_(INFO) << "WatcherEntityTest-end WatcherEntityTest_CloseNotifyFdLocked_004";
+}
+
+/**
+ * @tc.name: WatcherEntityTest_CloseNotifyFdLocked_005
+ * @tc.desc: Test function of WatcherEntityTest::CloseNotifyFdLocked interface for multi.
+ * @tc.size: MEDIUM
+ * @tc.type: FUNC
+ * @tc.level Level 1
+ */
+HWTEST_F(WatcherEntityTest, WatcherEntityTest_CloseNotifyFdLocked_005, testing::ext::TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "WatcherEntityTest-begin WatcherEntityTest_CloseNotifyFdLocked_005";
+
+    EventfdMock::DisableMock();
+    InotifyMock::DisableMock();
+    UnistdMock::DisableMock();
+    auto &watcher = FileWatcher::GetInstance();
+    watcher.InitNotify();
+    watcher.run_ = true;
+    watcher.reading_ = false;
+    watcher.watcherInfoSet_.clear();
+    EXPECT_GT(watcher.notifyFd_, -1);
+    EXPECT_GT(watcher.eventFd_, -1);
+    std::vector<std::thread> threads;
+    threads.reserve(TOTAL_THREADS);
+
+    for (int i = 0; i < TOTAL_THREADS; ++i) {
+        threads.emplace_back([&watcher]() {
+            int result = watcher.CloseNotifyFdLocked();
+            EXPECT_EQ(result, 0);
+        });
+    }
+
+    for (auto& t : threads) {
+        t.join();
+    }
+
+    EXPECT_EQ(watcher.notifyFd_, -1);
+    EXPECT_EQ(watcher.eventFd_, -1);
+    EXPECT_FALSE(watcher.run_);
+    EXPECT_FALSE(watcher.closed_);
+
+    GTEST_LOG_(INFO) << "WatcherEntityTest-end WatcherEntityTest_CloseNotifyFdLocked_005";
+}
+
+/**
+ * @tc.name: WatcherEntityTest_CloseNotifyFdLocked_006
+ * @tc.desc: Test function of WatcherEntityTest::CloseNotifyFdLocked interface for multi.
+ * @tc.size: MEDIUM
+ * @tc.type: FUNC
+ * @tc.level Level 1
+ */
+HWTEST_F(WatcherEntityTest, WatcherEntityTest_CloseNotifyFdLocked_006, testing::ext::TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "WatcherEntityTest-begin WatcherEntityTest_CloseNotifyFdLocked_006";
+
+    EventfdMock::DisableMock();
+    InotifyMock::DisableMock();
+    UnistdMock::DisableMock();
+    auto &watcher = FileWatcher::GetInstance();
+    watcher.InitNotify();
+    watcher.run_ = false;
+    watcher.reading_ = false;
+    EXPECT_GT(watcher.notifyFd_, -1);
+    EXPECT_GT(watcher.eventFd_, -1);
+    WatcherCallback callback = nullptr;
+    std::vector<std::thread> threads;
+    threads.reserve(TOTAL_THREADS);
+    
+    std::atomic<int> count(0);
+    std::atomic<bool> started(false);
+    for (int i = 0; i < TOTAL_THREADS; ++i) {
+        threads.emplace_back([&watcher, &count, &started]() {
+            count++;
+            while (!started && (count < TOTAL_THREADS || !watcher.run_)) {
+                std::this_thread::sleep_for(std::chrono::milliseconds(1));
+            }
+            started = true;
+            watcher.reading_ = false;
+            watcher.watcherInfoSet_.clear();
+            int result = watcher.CloseNotifyFdLocked();
+            EXPECT_EQ(result, 0);
+        });
+    }
+    UnistdMock::EnableMock();
+    auto uniMock = UnistdMock::GetMock();
+    auto pollMock = PollMock::GetMock();
+    EXPECT_CALL(*uniMock, read(testing::_, testing::_, testing::_)).WillRepeatedly(testing::Return(-1));
+    EXPECT_CALL(*pollMock, poll(testing::_, testing::_, testing::_))
+        .WillRepeatedly([&watcher](struct pollfd *fds, nfds_t n, int timeout) {
+            fds[1].revents = POLLIN;
+            return 1;
+        });
+    watcher.GetNotifyEvent(callback);
+
+    for (auto& t : threads) {
+        t.join();
+    }
+
+    testing::Mock::VerifyAndClearExpectations(pollMock.get());
+    EXPECT_EQ(watcher.notifyFd_, -1);
+    EXPECT_EQ(watcher.eventFd_, -1);
+    EXPECT_FALSE(watcher.run_);
+    EXPECT_FALSE(watcher.closed_);
+
+    GTEST_LOG_(INFO) << "WatcherEntityTest-end WatcherEntityTest_CloseNotifyFdLocked_006";
 }
 
 } // namespace OHOS::FileManagement::ModuleFileIO::Test
