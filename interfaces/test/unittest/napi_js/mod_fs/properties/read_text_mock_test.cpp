7747fc8d4ed8b1c12cfc913302726342f3dc0e9b
qiwendong8 <qiwendong@huawei.com>
2026-06-24 11:43:43 +0800

    update:read_text接口TDD补充
    Signed-off-by: qiwendong8 <qiwendong@huawei.com>

--- interfaces/test/unittest/napi_js/mod_fs/properties/read_text_mock_test.cpp
+++ interfaces/test/unittest/napi_js/mod_fs/properties/read_text_mock_test.cpp
@@ -108,6 +108,30 @@ HWTEST_F(ReadTextMockTest, ReadTextMockTest_Sync_001, testing::ext::TestSize.Lev
     GTEST_LOG_(INFO) << "ReadTextMockTest-end ReadTextMockTest_Sync_001";
 }
 
+/**
+ * @tc.name: ReadTextMockTest_Sync_002
+ * @tc.desc: Test function of ReadText::Sync interface for FAILURE when InitArgs fails.
+ * @tc.size: MEDIUM
+ * @tc.type: FUNC
+ * @tc.level Level 1
+ */
+HWTEST_F(ReadTextMockTest, ReadTextMockTest_Sync_002, testing::ext::TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "ReadTextMockTest-begin ReadTextMockTest_Sync_002";
+    napi_env env = reinterpret_cast<napi_env>(0x1000);
+    napi_callback_info info = reinterpret_cast<napi_callback_info>(0x1000);
+
+    auto libnMock = LibnMock::GetMock();
+    EXPECT_CALL(*libnMock, InitArgs(testing::_, testing::_)).WillOnce(testing::Return(false));
+    EXPECT_CALL(*libnMock, ThrowErr(testing::_));
+
+    auto res = ReadText::Sync(env, info);
+
+    testing::Mock::VerifyAndClearExpectations(libnMock.get());
+    EXPECT_EQ(res, nullptr);
+
+    GTEST_LOG_(INFO) << "ReadTextMockTest-end ReadTextMockTest_Sync_002";
+}
 } // namespace Test
 } // namespace ModuleFileIO
 } // namespace FileManagement
