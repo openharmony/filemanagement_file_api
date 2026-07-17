f51c633ac3a233d2e30f611192f7b9a180286ed2
姜小林 <jiangxiaolin11@huawei.com>
2026-07-17 11:53:45 +0800

    RustTest测试用例优化
    Co-Authored-By: Agent
    Change-Id: Ie8ae433ef7979b71bdcf9d06e1148b83f62f4cd3
    Signed-off-by: 姜小林 <jiangxiaolin11@huawei.com>

--- interfaces/test/unittest/class_file/rust_test.cpp
+++ interfaces/test/unittest/class_file/rust_test.cpp
@@ -57,11 +57,10 @@ void RustTest::TearDown(void)
 
 /**
  * @tc.name: RustTest_ReaderIterator_0001
- * @tc.desc: Test function of ReaderIterator() interface for SUCCESS.
+ * @tc.desc: Test function of ReaderIterator() interface for SUCCESS when opening an existing file.
  * @tc.size: MEDIUM
  * @tc.type: FUNC
  * @tc.level Level 1
- * @tc.require: AR000IGDNF
  */
 HWTEST_F(RustTest, RustTest_ReaderIterator_0001, testing::ext::TestSize.Level1)
 {
@@ -76,17 +75,16 @@ HWTEST_F(RustTest, RustTest_ReaderIterator_0001, testing::ext::TestSize.Level1)
 
 /**
  * @tc.name: RustTest_ReaderIterator_0002
- * @tc.desc: Test function of ReaderIterator() interface for SUCCESS.
+ * @tc.desc: Test function of ReaderIterator() interface for FAILURE when file does not exist.
  * @tc.size: MEDIUM
  * @tc.type: FUNC
  * @tc.level Level 1
- * @tc.require: AR000IGDNF
  */
 HWTEST_F(RustTest, RustTest_ReaderIterator_0002, testing::ext::TestSize.Level1)
 {
     GTEST_LOG_(INFO) << "RustTest-begin RustTest_ReaderIterator_0002";
 
-    char filePath[] = "/data/test/rust_false.txt";
+    char filePath[] = "/data/test/non_existent.txt";
     void *iter = ReaderIterator(filePath);
     ASSERT_TRUE(errno == 2);
     EXPECT_TRUE(iter == nullptr);
@@ -96,18 +94,17 @@ HWTEST_F(RustTest, RustTest_ReaderIterator_0002, testing::ext::TestSize.Level1)
 
 /**
  * @tc.name: RustTest_ReaderIterator_0003
- * @tc.desc: Test function of ReaderIterator() interface for SUCCESS.
+ * @tc.desc: Test function of ReaderIterator() interface for FAILURE when path is nullptr.
  * @tc.size: MEDIUM
  * @tc.type: FUNC
  * @tc.level Level 1
- * @tc.require: AR000IGDNF
  */
 HWTEST_F(RustTest, RustTest_ReaderIterator_0003, testing::ext::TestSize.Level1)
 {
     GTEST_LOG_(INFO) << "RustTest-begin RustTest_ReaderIterator_0003";
 
     void *iter = ReaderIterator(nullptr);
-    ASSERT_TRUE(errno = 22);
+    ASSERT_TRUE(errno == 22);
     EXPECT_TRUE(iter == nullptr);
 
     GTEST_LOG_(INFO) << "RustTest-end RustTest_ReaderIterator_0003";
@@ -115,11 +112,10 @@ HWTEST_F(RustTest, RustTest_ReaderIterator_0003, testing::ext::TestSize.Level1)
 
 /**
  * @tc.name: RustTest_NextLine_0001
- * @tc.desc: Test function of NextLine() interface for SUCCESS.
+ * @tc.desc: Test function of NextLine() interface for SUCCESS when reading the first line.
  * @tc.size: MEDIUM
  * @tc.type: FUNC
  * @tc.level Level 0
- * @tc.require: AR000IGDNF
  */
 HWTEST_F(RustTest, RustTest_NextLine_0001, testing::ext::TestSize.Level0)
 {
@@ -139,17 +135,16 @@ HWTEST_F(RustTest, RustTest_NextLine_0001, testing::ext::TestSize.Level0)
 
 /**
  * @tc.name: RustTest_NextLine_0002
- * @tc.desc: Test function of NextLine() interface for SUCCESS.
+ * @tc.desc: Test function of NextLine() interface for FAILURE when iterator is nullptr.
  * @tc.size: MEDIUM
  * @tc.type: FUNC
  * @tc.level Level 0
- * @tc.require: AR000IGDNF
  */
 HWTEST_F(RustTest, RustTest_NextLine_0002, testing::ext::TestSize.Level0)
 {
     GTEST_LOG_(INFO) << "RustTest-begin RustTest_NextLine_0002";
 
-    char filePath[] = "/data/test/false.txt";
+    char filePath[] = "/data/test/non_existent.txt";
     void *iter = ReaderIterator(filePath);
     Str *ret = NextLine(iter);
     EXPECT_TRUE(ret == nullptr);
@@ -160,11 +155,10 @@ HWTEST_F(RustTest, RustTest_NextLine_0002, testing::ext::TestSize.Level0)
 
 /**
  * @tc.name: RustTest_NextLine_0003
- * @tc.desc: Test function of NextLine() interface for SUCCESS.
+ * @tc.desc: Test function of NextLine() interface for FAILURE when iterator is nullptr.
  * @tc.size: MEDIUM
  * @tc.type: FUNC
  * @tc.level Level 1
- * @tc.require: AR000IGDNF
  */
 HWTEST_F(RustTest, RustTest_NextLine_0003, testing::ext::TestSize.Level1)
 {
@@ -179,11 +173,10 @@ HWTEST_F(RustTest, RustTest_NextLine_0003, testing::ext::TestSize.Level1)
 
 /**
  * @tc.name: RustTest_Lseek_0001
- * @tc.desc: Test function of Lseek() interface for SUCCESS.
+ * @tc.desc: Test function of Lseek() interface for SUCCESS when seeking from END.
  * @tc.size: MEDIUM
  * @tc.type: FUNC
  * @tc.level Level 0
- * @tc.require: AR000IGCS3
  */
 HWTEST_F(RustTest, RustTest_Lseek_0001, testing::ext::TestSize.Level0)
 {
@@ -202,11 +195,10 @@ HWTEST_F(RustTest, RustTest_Lseek_0001, testing::ext::TestSize.Level0)
 
 /**
  * @tc.name: RustTest_Lseek_0002
- * @tc.desc: Test function of Lseek() interface for SUCCESS.
+ * @tc.desc: Test function of Lseek() interface for SUCCESS when seeking from END then CURRENT.
  * @tc.size: MEDIUM
  * @tc.type: FUNC
  * @tc.level Level 0
- * @tc.require: AR000IGCS3
  */
 HWTEST_F(RustTest, RustTest_Lseek_0002, testing::ext::TestSize.Level0)
 {
@@ -228,11 +220,10 @@ HWTEST_F(RustTest, RustTest_Lseek_0002, testing::ext::TestSize.Level0)
 
 /**
  * @tc.name: RustTest_Lseek_0003
- * @tc.desc: Test function of Lseek() interface for SUCCESS.
+ * @tc.desc: Test function of Lseek() interface for SUCCESS when seeking from START.
  * @tc.size: MEDIUM
  * @tc.type: FUNC
  * @tc.level Level 0
- * @tc.require: AR000IGCS3
  */
 HWTEST_F(RustTest, RustTest_Lseek_0003, testing::ext::TestSize.Level0)
 {
@@ -251,11 +242,10 @@ HWTEST_F(RustTest, RustTest_Lseek_0003, testing::ext::TestSize.Level0)
 
 /**
  * @tc.name: RustTest_Lseek_0004
- * @tc.desc: Test function of Lseek() interface for SUCCESS.
+ * @tc.desc: Test function of Lseek() interface for FAILURE when offset is negative from START.
  * @tc.size: MEDIUM
  * @tc.type: FUNC
  * @tc.level Level 1
- * @tc.require: AR000IGCS3
  */
 HWTEST_F(RustTest, RustTest_Lseek_0004, testing::ext::TestSize.Level1)
 {
@@ -274,11 +264,10 @@ HWTEST_F(RustTest, RustTest_Lseek_0004, testing::ext::TestSize.Level1)
 
 /**
  * @tc.name: RustTest_Lseek_0005
- * @tc.desc: Test function of Lseek() interface for SUCCESS.
+ * @tc.desc: Test function of Lseek() interface for SUCCESS when seeking from START then CURRENT.
  * @tc.size: MEDIUM
  * @tc.type: FUNC
  * @tc.level Level 0
- * @tc.require: AR000IGCS3
  */
 HWTEST_F(RustTest, RustTest_Lseek_0005, testing::ext::TestSize.Level0)
 {
@@ -300,11 +289,10 @@ HWTEST_F(RustTest, RustTest_Lseek_0005, testing::ext::TestSize.Level0)
 
 /**
  * @tc.name: RustTest_Lseek_0006
- * @tc.desc: Test function of Lseek() interface for SUCCESS.
+ * @tc.desc: Test function of Lseek() interface for SUCCESS when seeking from END with positive offset.
  * @tc.size: MEDIUM
  * @tc.type: FUNC
  * @tc.level Level 0
- * @tc.require: AR000IGCS3
  */
 HWTEST_F(RustTest, RustTest_Lseek_0006, testing::ext::TestSize.Level0)
 {
@@ -323,18 +311,21 @@ HWTEST_F(RustTest, RustTest_Lseek_0006, testing::ext::TestSize.Level0)
 
 /**
  * @tc.name: RustTest_Lseek_0007
- * @tc.desc: Test function of Lseek() interface for SUCCESS.
+ * @tc.desc: Test function of Lseek() interface for FAILURE when fd is closed.
  * @tc.size: MEDIUM
  * @tc.type: FUNC
  * @tc.level Level 0
- * @tc.require: AR000IGCS3
  */
 HWTEST_F(RustTest, RustTest_Lseek_0007, testing::ext::TestSize.Level0)
 {
     GTEST_LOG_(INFO) << "RustTest-begin RustTest_Lseek_0007";
 
+    const char fileStr[] = "/data/test/rust_test.txt";
+    int fd = open(fileStr, O_RDWR);
+    ASSERT_TRUE(fd >= 0);
+    ASSERT_TRUE(close(fd) == 0);
     long long offset = 30;
-    long long ret = Lseek(34, offset, END);
+    long long ret = Lseek(fd, offset, END);
     EXPECT_TRUE(ret == -1);
 
     GTEST_LOG_(INFO) << "RustTest-end RustTest_Lseek_0007";
@@ -342,11 +333,10 @@ HWTEST_F(RustTest, RustTest_Lseek_0007, testing::ext::TestSize.Level0)
 
 /**
  * @tc.name: RustTest_Mkdirs_0001
- * @tc.desc: Test function of Mkdirs() interface for SUCCESS.
+ * @tc.desc: Test function of Mkdirs() interface for SUCCESS when creating a single directory.
  * @tc.size: MEDIUM
  * @tc.type: FUNC
  * @tc.level Level 0
- * @tc.require: AR000IGDNJ
  */
 HWTEST_F(RustTest, RustTest_Mkdirs_0001, testing::ext::TestSize.Level0)
 {
@@ -367,11 +357,10 @@ HWTEST_F(RustTest, RustTest_Mkdirs_0001, testing::ext::TestSize.Level0)
 
 /**
  * @tc.name: RustTest_Mkdirs_0002
- * @tc.desc: Test function of Mkdirs() interface for SUCCESS.
+ * @tc.desc: Test function of Mkdirs() interface for SUCCESS when creating multiple directories.
  * @tc.size: MEDIUM
  * @tc.type: FUNC
  * @tc.level Level 0
- * @tc.require: AR000IGDNJ
  */
 HWTEST_F(RustTest, RustTest_Mkdirs_0002, testing::ext::TestSize.Level0)
 {
@@ -392,11 +381,10 @@ HWTEST_F(RustTest, RustTest_Mkdirs_0002, testing::ext::TestSize.Level0)
 
 /**
  * @tc.name: RustTest_Mkdirs_0003
- * @tc.desc: Test function of Mkdirs() interface for SUCCESS.
+ * @tc.desc: Test function of Mkdirs() interface for FAILURE when parent directory does not exist.
  * @tc.size: MEDIUM
  * @tc.type: FUNC
  * @tc.level Level 0
- * @tc.require: AR000IGDNJ
  */
 HWTEST_F(RustTest, RustTest_Mkdirs_0003, testing::ext::TestSize.Level0)
 {
@@ -413,11 +401,10 @@ HWTEST_F(RustTest, RustTest_Mkdirs_0003, testing::ext::TestSize.Level0)
 
 /**
  * @tc.name: RustTest_Mkdirs_0004
- * @tc.desc: Test function of Mkdirs() interface for SUCCESS.
+ * @tc.desc: Test function of Mkdirs() interface for SUCCESS when directory already exists.
  * @tc.size: MEDIUM
  * @tc.type: FUNC
  * @tc.level Level 0
- * @tc.require: AR000IGDNJ
  */
 HWTEST_F(RustTest, RustTest_Mkdirs_0004, testing::ext::TestSize.Level0)
 {
@@ -432,11 +419,10 @@ HWTEST_F(RustTest, RustTest_Mkdirs_0004, testing::ext::TestSize.Level0)
 
 /**
  * @tc.name: RustTest_Mkdirs_0005
- * @tc.desc: Test function of Mkdirs() interface for SUCCESS.
+ * @tc.desc: Test function of Mkdirs() interface for FAILURE when path is empty.
  * @tc.size: MEDIUM
  * @tc.type: FUNC
  * @tc.level Level 0
- * @tc.require: AR000IGDNJ
  */
 HWTEST_F(RustTest, RustTest_Mkdirs_0005, testing::ext::TestSize.Level0)
 {
@@ -452,11 +438,10 @@ HWTEST_F(RustTest, RustTest_Mkdirs_0005, testing::ext::TestSize.Level0)
 
 /**
  * @tc.name: RustTest_Mkdirs_0006
- * @tc.desc: Test function of Mkdirs() interface for SUCCESS.
+ * @tc.desc: Test function of Mkdirs() interface for FAILURE when path is empty.
  * @tc.size: MEDIUM
  * @tc.type: FUNC
  * @tc.level Level 0
- * @tc.require: AR000IGDNJ
  */
 HWTEST_F(RustTest, RustTest_Mkdirs_0006, testing::ext::TestSize.Level0)
 {
@@ -472,11 +457,10 @@ HWTEST_F(RustTest, RustTest_Mkdirs_0006, testing::ext::TestSize.Level0)
 
 /**
  * @tc.name: RustTest_Mkdirs_0007
- * @tc.desc: Test function of Mkdirs() interface for SUCCESS.
+ * @tc.desc: Test function of Mkdirs() interface for FAILURE when path already exists as a file.
  * @tc.size: MEDIUM
  * @tc.type: FUNC
  * @tc.level Level 0
- * @tc.require: AR000IGDNJ
  */
 HWTEST_F(RustTest, RustTest_Mkdirs_0007, testing::ext::TestSize.Level0)
 {
@@ -492,11 +476,10 @@ HWTEST_F(RustTest, RustTest_Mkdirs_0007, testing::ext::TestSize.Level0)
 
 /**
  * @tc.name: RustTest_Mkdirs_0008
- * @tc.desc: Test function of Mkdirs() interface for SUCCESS.
+ * @tc.desc: Test function of Mkdirs() interface for FAILURE when path already exists as a file.
  * @tc.size: MEDIUM
  * @tc.type: FUNC
  * @tc.level Level 0
- * @tc.require: AR000IGDNJ
  */
 HWTEST_F(RustTest, RustTest_Mkdirs_0008, testing::ext::TestSize.Level0)
 {
@@ -512,11 +495,10 @@ HWTEST_F(RustTest, RustTest_Mkdirs_0008, testing::ext::TestSize.Level0)
 
 /**
  * @tc.name: RustTest_GetParent_0001
- * @tc.desc: Test function of GetParent() interface for SUCCESS.
+ * @tc.desc: Test function of GetParent() interface for SUCCESS when fd is valid.
  * @tc.size: MEDIUM
  * @tc.type: FUNC
  * @tc.level Level 0
- * @tc.require: AR000IGDNL
  */
 HWTEST_F(RustTest, RustTest_GetParent_0001, testing::ext::TestSize.Level0)
 {
@@ -536,11 +518,10 @@ HWTEST_F(RustTest, RustTest_GetParent_0001, testing::ext::TestSize.Level0)
 
 /**
  * @tc.name: RustTest_GetParent_0002
- * @tc.desc: Test function of GetParent() interface for SUCCESS.
+ * @tc.desc: Test function of GetParent() interface for FAILURE when fd is invalid.
  * @tc.size: MEDIUM
  * @tc.type: FUNC
  * @tc.level Level 1
- * @tc.require: AR000IGDNL
  */
 HWTEST_F(RustTest, RustTest_GetParent_0002, testing::ext::TestSize.Level1)
 {
@@ -559,7 +540,6 @@ HWTEST_F(RustTest, RustTest_GetParent_0002, testing::ext::TestSize.Level1)
  * @tc.size: MEDIUM
  * @tc.type: FUNC
  * @tc.level Level 1
- * @tc.require: AR000IGDNL
  */
 HWTEST_F(RustTest, RustTest_GetParent_0003, testing::ext::TestSize.Level1)
 {
@@ -578,11 +558,10 @@ HWTEST_F(RustTest, RustTest_GetParent_0003, testing::ext::TestSize.Level1)
 
 /**
  * @tc.name: RustTest_StrFree_0001
- * @tc.desc: Test function of StrFree() interface for SUCCESS.
+ * @tc.desc: Test function of StrFree() interface for SUCCESS when freeing a valid str.
  * @tc.size: MEDIUM
  * @tc.type: FUNC
  * @tc.level Level 1
- * @tc.require: AR000IGDNF
  */
 HWTEST_F(RustTest, RustTest_StrFree_0001, testing::ext::TestSize.Level1)
 {
@@ -601,11 +580,10 @@ HWTEST_F(RustTest, RustTest_StrFree_0001, testing::ext::TestSize.Level1)
 
 /**
  * @tc.name: RustTest_StrFree_0002
- * @tc.desc: Test function of StrFree() interface for SUCCESS.
+ * @tc.desc: Test function of StrFree() interface for SUCCESS when str is nullptr.
  * @tc.size: MEDIUM
  * @tc.type: FUNC
  * @tc.level Level 1
- * @tc.require: AR000IGDNF
  */
 HWTEST_F(RustTest, RustTest_StrFree_0002, testing::ext::TestSize.Level1)
 {
@@ -619,30 +597,29 @@ HWTEST_F(RustTest, RustTest_StrFree_0002, testing::ext::TestSize.Level1)
 }
 
 /**
- * @tc.name: RustTest_CutFileName_0000
- * @tc.desc: Test function of CutFileName() interface for SUCCESS.
+ * @tc.name: RustTest_CutFileName_0001
+ * @tc.desc: Test function of CutFileName() interface for SUCCESS when cutting file name.
  * @tc.size: MEDIUM
  * @tc.type: FUNC
  * @tc.level Level 1
- * @tc.require: AR000IGDNF
  */
-HWTEST_F(RustTest, RustTest_CutFileName_0000, testing::ext::TestSize.Level1)
+HWTEST_F(RustTest, RustTest_CutFileName_0001, testing::ext::TestSize.Level1)
 {
-    GTEST_LOG_(INFO) << "RustTest-begin RustTest_CutFileName_0000";
+    GTEST_LOG_(INFO) << "RustTest-begin RustTest_CutFileName_0001";
     string myStr = "<?,你好，世界！hello, world! ?XML, 你好，世界！";
 
     Str *str1 = CutFileName(myStr.c_str(), 2);
-    GTEST_LOG_(INFO) << "RustTest_CutFileName_0000 str1" << str1->str;
-    GTEST_LOG_(INFO) << "RustTest_CutFileName_0000 str1" << str1->len;
+    GTEST_LOG_(INFO) << "RustTest_CutFileName_0001 str1" << str1->str;
+    GTEST_LOG_(INFO) << "RustTest_CutFileName_0001 str1" << str1->len;
     EXPECT_TRUE(string(str1->str).compare("<?,你好，世界！hello, world! ?XML, 你好，世") == 0);
     StrFree(str1);
     Str *str2 = CutFileName(myStr.c_str(), 10);
-    GTEST_LOG_(INFO) << "RustTest_CutFileName_0000 str2" << str2->str;
-    GTEST_LOG_(INFO) << "RustTest_CutFileName_0000 str2" << str2->len;
+    GTEST_LOG_(INFO) << "RustTest_CutFileName_0001 str2" << str2->str;
+    GTEST_LOG_(INFO) << "RustTest_CutFileName_0001 str2" << str2->len;
     EXPECT_TRUE(string(str2->str).compare("<?,你好，世界！hello, world! ?X") == 0);
     StrFree(str2);
 
-    GTEST_LOG_(INFO) << "RustTest-end RustTest_CutFileName_0000";
+    GTEST_LOG_(INFO) << "RustTest-end RustTest_CutFileName_0001";
 }
 
 } // namespace ModuleFileIO
